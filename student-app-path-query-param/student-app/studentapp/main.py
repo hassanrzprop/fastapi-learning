496d02f3be5c4837c50038bda38e0211e088ef42
Hassan Raza <141661553+hassanrzprop@users.noreply.github.com>
2024-07-21 19:24:56 +0500

    udated function added and student app is completed

--- student-app-path-query-param/student-app/studentapp/main.py
+++ student-app-path-query-param/student-app/studentapp/main.py
@@ -1,16 +1,36 @@
 from fastapi import FastAPI
 import uvicorn
 app=FastAPI()
-student=[{"rollNo":'234',"userName":"Ali"}]
+
+students = [
+    {"rollNo": '234', "userName": "Ali"},
+    {"rollNo": '324', "userName": "waqar"},
+    {"rollNo": '3878', "userName": "haider"},
+    {"rollNo": '312', "userName": "huzaifa"}
+]
+
+
 @app.get("/")
 def showStudents():
-    print("student list called",student)
-    return student
+    print("student list called",students)
+    return students
 @app.get("/addStudent/")
 def getTodos(rollNo: str,userName: str):
     dicion={"rollNo":rollNo,"userName":userName}
-    student.append(dicion)
+    students.append(dicion)
     print("getTodos called",rollNo,userName)
     return rollNo+userName;
+
+@app.get("/updateStudent/{oldRollNo}/{newRollNo}/{newUserName}")
+def update_student(oldRollNo: str, newRollNo: str, newUserName: str):
+    for student in students:
+        if student["rollNo"] == oldRollNo:
+            student["rollNo"] = newRollNo
+            student["userName"] = newUserName
+            return {"message": f"Student {oldRollNo} updated successfully to {newRollNo} and {newUserName}"}
+    return {"error": "Student not found"}
 def start():
     uvicorn.run("studentapp.main:app",host="127.0.0.1",port=8080,reload=True) 
+
+
+
